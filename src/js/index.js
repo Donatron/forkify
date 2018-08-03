47afbdd1e34d5e691f2d88578ac062fb7b04aa4f
Donatron <don.macarthur68@gmail.com>
2018-08-03 12:55:31 +1000

    setup babel
    added mvc for search fields

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,32 @@
-// Global app controller
-import num from './test';
+import Search from './models/Search';
 
-console.log(`I imported ${num} from another test.js`);
+/** Global state of the app
+* - Search object
+* - CUrrent recipe object
+* - Shopping list object
+* - Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+  // 1. Get query from the view
+  const query = document.querySelector('.search__field').value; //TODOList
+
+  if (query) {
+    // 2. New search object and add to state
+    state.search = new Search(query);
+
+    // 3. Prepare UI for results
+
+    // 4. Search for recipes
+    await state.search.getResults();
+
+    // 5. Render results on UI
+    console.log(state.search.result);
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'e1a3307cd7d4a6406fa9d9309b2082b2';
+    try {
+      const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes
+      // console.log(this.result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
